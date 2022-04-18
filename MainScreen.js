45e0f0284b0211edefb8e223f8c3de6f4b94a1e2
Shifaan <53972679+Shifaan0010@users.noreply.github.com>
2022-04-18 20:05:53 +0530

    Added 'react-native-tcp-socket', ran expo prebuild and eas build -p android --profile preview

--- MainScreen.js
+++ MainScreen.js
@@ -1,21 +1,21 @@
-import { View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { useState } from 'react';
+import TcpSocket from 'react-native-tcp-socket';
 
 export default function MainScreen() {
-    const [led, setLed] = useState({led1: 0, led2: 0});
+    const [led, setLed] = useState(0);
 
-    const ledPress = (led_num) => {
-        if(led_num == 1){
-            setLed({
-                led1: !led.led1,
-                led2: led.led2,
-            });
+    const client = TcpSocket.createConnection(
+        {host: '192.168.4.1', port: 80}, 
+        () => {
+            client.write('Hello World\r\n');
         }
-        else if(led_num == 2){
-            setLed({
-                led1: led.led1,
-                led2: !led.led2,
-            });
+    );
+
+    const ledPress = () => {
+        if (client.readyState == 'open') {
+            client.write(led == 0 ? 'LED:ON\r\n' : led == 1 ? 'LED:AUTO\r\n' : 'LED:OFF\r\n');
+            setLed((led + 1) % 3);
         }
 
         // fetch("http://192.168.117.1:8081/", {
@@ -27,22 +27,20 @@ export default function MainScreen() {
         //   })
         //     .then(res => console.log(res))
         //     .catch(err => console.log(err));
-    }
+    };
+
     return (
         <View style={styles.container}>
-            <TouchableOpacity 
-                style={led.led1? [styles.button, styles.ledOn]: [styles.button, styles.ledOff]} 
-                onPress={() => ledPress(1)}
+            <TouchableOpacity
+                style={
+                    led == 0
+                        ? [styles.button, styles.ledOff]
+                        : led == 1 ? [styles.button, styles.ledOn] : [styles.button, styles.ledauto]
+                }
+                onPress={() => ledPress()}
             >
-                <Text style={styles.heading}>LED 1</Text>
-                <Text style={styles.subheading}>{led.led1 ? "ON" : "OFF"}</Text>
-            </TouchableOpacity>
-            <TouchableOpacity 
-                style={led.led2? [styles.button, styles.ledOn]: [styles.button, styles.ledOff]} 
-                onPress={() => ledPress(2)}
-            >
-                <Text style={styles.heading}>LED 2</Text>
-                <Text style={styles.subheading}>{led.led2 ? "ON" : "OFF"}</Text>
+                <Text style={styles.heading}>LED</Text>
+                <Text style={styles.subheading}>{led == 0 ? 'OFF' : led == 1 ? 'ON' : 'AUTO'}</Text>
             </TouchableOpacity>
         </View>
     );
@@ -51,23 +49,26 @@ export default function MainScreen() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "space-evenly",
-        alignItems: "center",
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
     },
     button: {
         width: '75%',
         height: '30%',
-        borderColor: "black",
+        borderColor: 'black',
         borderWidth: 2,
-        alignItems: "center",
-        justifyContent: "center",
+        alignItems: 'center',
+        justifyContent: 'center',
         borderRadius: 20,
     },
     ledOn: {
-        backgroundColor: "#EB581E",
+        backgroundColor: '#EB581E',
     },
     ledOff: {
-        backgroundColor: "#009C72",
+        backgroundColor: '#009C72',
+    },
+    ledauto: {
+        backgroundColor: '#AA9C72',
     },
     heading: {
         fontSize: 60,
@@ -75,4 +76,4 @@ const styles = StyleSheet.create({
     subheading: {
         fontSize: 30,
     },
-})
+});
